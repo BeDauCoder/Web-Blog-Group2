4cd9c220ddfa09bef4d04fbd1e56becbcc5598d7
BeDauCoder <sangnguyen29052004@gmail.com>
2024-10-08 22:42:06 +0700

    commit

--- web_project/WebBlog/models.py
+++ web_project/WebBlog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class User(models.Model):
+    id_user = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+    birth_date = models.DateField(null=True, blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
